6f7e9f493620db38555443a5ed61651c7952942c
nra4ever <muerteproductions@gmail.com>
2022-11-04 04:58:19 +0000

    change quotes

--- apps/calremind/calremind.py
+++ apps/calremind/calremind.py
@@ -6,37 +6,37 @@ import json
 class CalHandler(hass.Hass):
     def initialize(self):
         #HA Server IP
-        self.serverip = self.args['server_ip']
+        self.serverip = self.args["server_ip"]
         #HA Port
         port = "8123"
-        if self.args['ha_port']:
-            port = self.args['ha_port']
+        if self.args["ha_port"]:
+            port = self.args["ha_port"]
         self.serverport = port
         #Long lived access token from HA
-        self.apikey = self.args['ha_token']
+        self.apikey = self.args["ha_token"]
         #entity ID of HA calendar
-        self.calID = self.args['calendar_id']
+        self.calID = self.args["calendar_id"]
         #max number of upcoming events to handle
         maxe = 3
-        if self.args['max_events']:
-            maxe = self.args['max_events']
+        if self.args["max_events"]:
+            maxe = self.args["max_events"]
         self.maxEvents = maxe
         #how many hours before appointment should event be added to sensor
         haway = 48
-        if self.arges['hours_away']:
-            haway = self.args['hours_away']
+        if self.arges["hours_away"]:
+            haway = self.args["hours_away"]
         self.hoursaway = haway
         #second field (1 in the default case) represents minute past the hour the script will run
         rt = time(00, 29, 0)
         #changing the "sensor.calremind" value below will allow you to change the name of the generated sensor
-        sens = 'sensor.calremind'
-        if self.args['sensor_id']:
-            sens = self.args['sensor_id']        
+        sens = "sensor.calremind"
+        if self.args["sensor_id"]:
+            sens = self.args["sensor_id"]        
         self.calremind = self.get_entity(sens)
         #Changing this value to 1 will make sensor attribute indices begin at 1 instead of 
         ioff = 0
-        if self.arges['index_offset']:
-            ioff = self.args['index_offset']
+        if self.arges["index_offset"]:
+            ioff = self.args["index_offset"]
         self.indexOffset = ioff
         #callback hook
         self.run_hourly(self.calcheck, rt)
@@ -68,7 +68,7 @@ class CalHandler(hass.Hass):
                 #get current index
                 i = self.eventArr.index(appointment)
                 #format time and date for sensor
-                appTime = appointment['start']['dateTime'][:-9]
+                appTime = appointment["start"]["dateTime"][:-9]
                 ato = datetime.strptime(appTime, "%Y-%m-%dT%H:%M")
                 appDate_hr = datetime.strftime(ato, "%a %d %b")
                 #if location is provided, load it into location variable
@@ -81,7 +81,7 @@ class CalHandler(hass.Hass):
                     "event{}".format(str(i + self.indexOffset)): "on",
                     "name{}".format(str(i + self.indexOffset)): appointment["summary"],
                     "date{}".format(str(i + self.indexOffset)): appDate_hr,
-                    "time{}".format(str(i + self.indexOffset)): appTime.split('T')[1],
+                    "time{}".format(str(i + self.indexOffset)): appTime.split("T")[1],
                     "location{}".format(str(i + self.indexOffset)): location
                 })
             #if list under maximum, set higher states to off
